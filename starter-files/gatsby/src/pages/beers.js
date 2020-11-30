010b1bf6e8a8815e9b48dfbf46d6f40b6bb6b959
Bri997 <brian.musial@gmail.com>
2020-11-30 16:26:27 -0600

    adding pages to Gatsby

---
+++ starter-files/gatsby/src/pages/beers.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function BeersPage() {
+  return (
+    <div>
+      <h1>Hey Beers Page...</h1>
+    </div>
+  );
+}
---
+++ starter-files/gatsby/src/pages/slicemasters.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SlicemastersPage() {
+  return (
+    <div>
+      <h1>Hey Slicemasters Page...</h1>
+    </div>
+  );
+}
