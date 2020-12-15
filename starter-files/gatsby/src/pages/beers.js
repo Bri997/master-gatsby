32106093cfc95b7ea3ce07ce7b255e04522ac9b3
Brian Musial <brian.musial@gmail.com>
2020-12-15 15:30:25 -0500

    adding layout

--- starter-files/gatsby/src/pages/beers.js
+++ starter-files/gatsby/src/pages/beers.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import Nav from '../components/Nav'
+import React from 'react';
+import Nav from '../components/Nav';
+
 export default function BeersPage() {
   return (
     <div>
-      <Nav></Nav>
+      <Nav />
       <h1>Hey Beers Page...</h1>
-
 
       <div>
         <h1>a</h1>
       </div>
     </div>
-  )
+  );
 }
--- starter-files/gatsby/src/pages/index.js
+++ starter-files/gatsby/src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Nav from "../components/Nav"
+import Layout from '../components/Layout';
+import Nav from '../components/Nav';
 
 export default function HomePage() {
   return (
-    <div><Nav/>
+    <Layout>
+      <Nav />
       <h1>Hi there</h1>
-    </div>
+    </Layout>
   );
 }
